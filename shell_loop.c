57235b8ed25442c905565440ebcc01ad6b1b6b56
efodthe2nd <davidefod@gmail.com>
2022-11-14 13:15:11 +0000

    shell_loop

--- shell_loop.c
+++ shell_loop.c
@@ -44,7 +44,7 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - finds a builtin command
+ * find_builtin - finds a builtin command in simple shell
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
